ff4e347f5947e55a152c0e6d7e7582f3750e68c7
KONSTANTIN KAIMOV <ikaimov98@gmail.com>
2024-05-02 17:02:29 +0200

    working pipes

--- srcs/executor/exec.c
+++ srcs/executor/exec.c
@@ -1,47 +1,62 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   exec.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kokaimov <kokaimov@student.42berlin.de>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/02 02:47:33 by kokaimov          #+#    #+#             */
-/*   Updated: 2024/05/02 03:36:08 by kokaimov         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/wait.h>
-
-#define MAX_ARGS 10
+#include "../../includes/minishell.h"
+#include <sys/stat.h>
 
 typedef struct {
 	char *command;
-	char *args[MAX_ARGS];
-} Command;
+	char *args[10];
+}	Command;
 
-void	execute_command(Command cmd)
-{
-	int	status;
-	pid_t	pid = fork();
+static char	*search_executable(char *program, char **path_parts) {
+	struct stat statbuf;
+
+	if (ft_strchr(program, '/'))
+		return (access(program, X_OK) == 0) ? ft_strdup(program) : NULL;
+
+	while (path_parts && *path_parts)
+	{
+		char *path = ft_strjoin(*path_parts, "/");
+		char *executable_path = ft_strjoin(path, program);
+		free(path);
+		if (access(executable_path, X_OK) == 0 && stat(executable_path, &statbuf) == 0 && S_ISREG(statbuf.st_mode))
+			return (executable_path);
+		free(executable_path);
+		path_parts++;
+	}
+	return (NULL);
+}
+
+void	execute_command(Command cmd) {
+	int status;
+	char **path_parts = ft_split(getenv("PATH"), ':');
+	char *executable_path = search_executable(cmd.command, path_parts);
+	if (!executable_path)
+	{
+		write(STDERR_FILENO, "Command not found\n", 18);
+		free_matrix(path_parts);
+		return ;
+	}
+
+	pid_t pid = fork();
 	if (pid == 0)
 	{
-		if (execve(cmd.command, cmd.args, NULL) == -1)
-		{
-			write(STDERR_FILENO, "Error executing command\n", 24);
-			exit(EXIT_FAILURE);
-		}
+		execve(executable_path, cmd.args, NULL);
+		perror("execve");
+		exit(EXIT_FAILURE);
 	}
 	else if (pid > 0)
 		waitpid(pid, &status, 0);
 	else
-		write(STDERR_FILENO, "Fork failed\n", 12);
+		perror("fork");
+	free(executable_path);
+	free_matrix(path_parts);
 }
 
-void execute_pipeline(Command *cmds, int n) {
+void	execute_pipeline(Command *cmds, int n) {
 	int i = 0, in_fd = 0, fd[2];
 
 	while (i < n)
@@ -50,7 +65,7 @@ void execute_pipeline(Command *cmds, int n) {
 		{
 			if (pipe(fd) < 0)
 			{
-				write(STDERR_FILENO, "Pipe failed\n", 12);
+				perror("pipe");
 				exit(EXIT_FAILURE);
 			}
 		}
@@ -60,42 +75,46 @@ void execute_pipeline(Command *cmds, int n) {
 		{
 			if (in_fd != 0)
 			{
-				dup2(in_fd, 0); // Set stdin to the input part of the pipe
+				dup2(in_fd, 0);  // Use previous pipe's read end as stdin
 				close(in_fd);
 			}
 			if (i < n - 1)
 			{
-				close(fd[0]); // Close the read end of the pipe in the child
-				dup2(fd[1], 1); // Set stdout to the output part of the pipe
+				close(fd[0]);  // Close the read end of the next pipe
+				dup2(fd[1], 1);  // Use the write end of the next pipe as stdout
 				close(fd[1]);
 			}
-			execve(cmds[i].command, cmds[i].args, NULL);
-			write(STDERR_FILENO, "Execve failed\n", 14);
-			exit(EXIT_FAILURE);
+
+			execute_command(cmds[i]);
+			exit(EXIT_FAILURE);  // Should never reach here unless execution fails
 		}
 		else if (pid < 0)
-			return ((void)write(STDERR_FILENO, "Fork failed\n", 12));
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
 		else
 		{
 			if (in_fd != 0)
-				close(in_fd);
+				close(in_fd);  // Close the previous pipe's read end
 			if (i < n - 1)
 			{
-				close(fd[1]); // Close the write end of the pipe in the parent
-				in_fd = fd[0]; // Keep the read end of the pipe
+				close(fd[1]);  // Close the current pipe's write end
+				in_fd = fd[0];  // Keep the read end to pass as stdin to next command
 			}
 		}
 		i++;
 	}
+
 	while (wait(NULL) > 0);
 }
 
 int	main()
 {
 	Command cmds[3] = {
-			{"/bin/ls", {"/bin/ls", "-l", NULL}},
-			{"/usr/bin/grep", {"/usr/bin/grep", "minishell", NULL}},
-			{"/usr/bin/sort", {"/usr/bin/sort", NULL}}
+			{"ls", {"ls", "-l", "-a", NULL}},
+			{"grep", {"grep", "minishell", NULL}},
+			{"sort", {"sort", NULL}}
 	};
 
 	execute_pipeline(cmds, 3);
