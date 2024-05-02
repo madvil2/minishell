a961aad48b22d4af628e07daffa8ba0708ecf883
KONSTANTIN KAIMOV <ikaimov98@gmail.com>
2024-05-02 03:37:39 +0200

    save

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: kokaimov <kokaimov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 04:49:31 by kokaimov          #+#    #+#             */
-/*   Updated: 2024/04/27 04:57:25 by kokaimov         ###   ########.fr       */
+/*   Updated: 2024/05/02 03:05:10 by kokaimov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define MINISHELL_H
 
 # include <stdio.h>
+# include "../libft/includes/libft.h"
 
 #endif
